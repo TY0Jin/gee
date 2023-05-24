63b7a572fd2946bbc73a5ae5a1b1e02e5fae6b35
jinxw <jinxingw@qq.com>
2023-05-25 00:15:36 +0800

    feat(gee):分组路由

--- main.go
+++ main.go
@@ -10,20 +10,26 @@ func main() {
 	engine.GET("/", func(c *gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	engine.GET("/hello", func(c *gee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-	engine.GET("/hello/:name", func(c *gee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-	engine.POST("/login", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+	v1 := engine.Group("/v1")
+	{
+		v1.GET("/hello", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 		})
-	})
-	engine.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
-	})
+		v1.GET("/hello/:name", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+	}
+	v2 := engine.Group("v2")
+	{
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+		v2.GET("/assets/*filepath", func(c *gee.Context) {
+			c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+		})
+	}
 	engine.RUN(":9999")
 }
